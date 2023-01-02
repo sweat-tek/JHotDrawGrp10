72491bb1a7a06e5656556cbaf52509cbe6a9c1a7
tobber00 <39513708+tobber00@users.noreply.github.com>
2023-01-02 20:32:15 +0100

    reduced if statement

--- jhotdraw-samples/jhotdraw-samples-misc/src/main/java/org/jhotdraw/samples/svg/figures/SVGPathFigure.java
+++ jhotdraw-samples/jhotdraw-samples-misc/src/main/java/org/jhotdraw/samples/svg/figures/SVGPathFigure.java
@@ -79,35 +79,34 @@ public class SVGPathFigure extends AbstractAttributedCompositeFigure implements 
     public void draw(Graphics2D g) {
         double opacity = get(OPACITY);
         opacity = Math.min(Math.max(0d, opacity), 1d);
-        if (opacity != 0d) {
-            if (opacity != 1d) {
-                Rectangle2D.Double drawingArea = getDrawingArea();
-                Rectangle2D clipBounds = g.getClipBounds();
-                if (clipBounds != null) {
-                    Rectangle2D.intersect(drawingArea, clipBounds, drawingArea);
-                }
-                if (!drawingArea.isEmpty()) {
-                    BufferedImage buf = new BufferedImage(
-                            Math.max(1, (int) ((2 + drawingArea.width) * g.getTransform().getScaleX())),
-                            Math.max(1, (int) ((2 + drawingArea.height) * g.getTransform().getScaleY())),
-                            BufferedImage.TYPE_INT_ARGB);
-                    Graphics2D gr = buf.createGraphics();
-                    gr.scale(g.getTransform().getScaleX(), g.getTransform().getScaleY());
-                    gr.translate((int) -drawingArea.x, (int) -drawingArea.y);
-                    gr.setRenderingHints(g.getRenderingHints());
-                    drawFigure(gr);
-                    gr.dispose();
-                    Composite savedComposite = g.getComposite();
-                    g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, (float) opacity));
-                    g.drawImage(buf, (int) drawingArea.x, (int) drawingArea.y,
-                            2 + (int) drawingArea.width, 2 + (int) drawingArea.height, null);
-                    g.setComposite(savedComposite);
-                }
-            } else {
-                drawFigure(g);
+        if (opacity != 0d && opacity != 1d) {
+            Rectangle2D.Double drawingArea = getDrawingArea();
+            Rectangle2D clipBounds = g.getClipBounds();
+            if (clipBounds != null) {
+                Rectangle2D.intersect(drawingArea, clipBounds, drawingArea);
             }
+            if (!drawingArea.isEmpty()) {
+                BufferedImage buf = new BufferedImage(
+                        Math.max(1, (int) ((2 + drawingArea.width) * g.getTransform().getScaleX())),
+                        Math.max(1, (int) ((2 + drawingArea.height) * g.getTransform().getScaleY())),
+                        BufferedImage.TYPE_INT_ARGB);
+                Graphics2D gr = buf.createGraphics();
+                gr.scale(g.getTransform().getScaleX(), g.getTransform().getScaleY());
+                gr.translate((int) -drawingArea.x, (int) -drawingArea.y);
+                gr.setRenderingHints(g.getRenderingHints());
+                drawFigure(gr);
+                gr.dispose();
+                Composite savedComposite = g.getComposite();
+                g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, (float) opacity));
+                g.drawImage(buf, (int) drawingArea.x, (int) drawingArea.y,
+                        2 + (int) drawingArea.width, 2 + (int) drawingArea.height, null);
+                g.setComposite(savedComposite);
+            }
+        } else if (opacity != 0d) {
+            drawFigure(g);
         }
     }
+
 
     @Override
     public void drawFigure(Graphics2D g) {
