06f9831c248e04e8553ebff3109df8ac0583c528
Rasmus Meldgaard <MrKetuz@users.noreply.github.com>
2023-01-02 20:03:10 +0100

    Created unit test

---
+++ jhotdraw-samples/jhotdraw-samples-misc/src/test/java/org/jhotdraw/samples/svg/SVGDrawingPanelTest.java
@@ -0,0 +1,47 @@
+package org.jhotdraw.samples.svg;
+
+import org.jhotdraw.draw.Drawing;
+import org.jhotdraw.draw.io.*;
+import org.jhotdraw.samples.svg.figures.SVGImageFigure;
+import org.jhotdraw.samples.svg.figures.SVGTextFigure;
+import org.jhotdraw.samples.svg.io.ImageMapOutputFormat;
+import org.jhotdraw.samples.svg.io.SVGOutputFormat;
+import org.jhotdraw.samples.svg.io.SVGZInputFormat;
+import org.jhotdraw.samples.svg.io.SVGZOutputFormat;
+import org.junit.Test;
+
+import java.awt.image.BufferedImage;
+import java.util.LinkedList;
+
+import static org.junit.Assert.*;
+public class SVGDrawingPanelTest {
+
+    @Test
+    public void checkInput(){
+        SVGDrawingPanel panel = new SVGDrawingPanel();
+        Drawing drawing = panel.createDrawing();
+        LinkedList list = (LinkedList) drawing.getInputFormats();
+
+        assertNotNull(drawing.getInputFormats());
+
+        assertEquals(list.size(), 5);
+
+        assertEquals(drawing, panel.createDrawing());
+    }
+
+    @Test
+    public void checkOutput(){
+
+        SVGDrawingPanel panel = new SVGDrawingPanel();
+        Drawing drawing = panel.createDrawing();
+        LinkedList list = (LinkedList) drawing.getOutputFormats();
+
+        assertNotNull(drawing.getOutputFormats());
+
+        assertEquals(list.size(), 6);
+
+        assertEquals(drawing, panel.createDrawing());
+
+    }
+
+}
