0028bddc22a593d32a7ebc76f7f49b78b323ec85
KappaRoss1 <71713872+KappaRoss1@users.noreply.github.com>
2023-01-02 16:19:48 +0100

    Unit Test for Getter and dependcy import in core module pom

---
+++ jhotdraw-core/src/test/java/org/jhotdraw/draw/tool/DelegationSelectionToolTest.java
@@ -0,0 +1,59 @@
+package org.jhotdraw.draw.tool;
+
+
+import org.jhotdraw.draw.DefaultDrawingEditor;
+import org.jhotdraw.draw.action.AlignAction;
+import org.junit.Test;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static org.junit.Assert.*;
+
+public class DelegationSelectionToolTest {
+
+   @Test
+    public void TestGetDrawingAction(){
+
+        //New instantiation of DelegationSelectionTool
+        DelegationSelectionTool selectionTool = new DelegationSelectionTool();
+
+        // Instantiation of new Collection of Actions
+        Collection<Action> actions = new ArrayList<Action>();
+        Action action = new AlignAction.Horizontal(new DefaultDrawingEditor());
+        actions.add(action);
+
+       // Check if Action does not exist in current Collection
+       assertTrue(!selectionTool.getDrawingActions().contains(action));
+
+       // insert Collections of Actions in DelegationSelectionTool
+        selectionTool.setDrawingActions(actions);
+
+        // Check if the Action exist in the Collection
+        assertTrue(selectionTool.getDrawingActions().contains(action));
+    }
+
+    @Test
+    public void TestGetFigureAction(){
+
+       //New instantiation of DelegationSelectionTool
+       DelegationSelectionTool selectionTool = new DelegationSelectionTool();
+
+       // Instantiation of new Collection of Actions
+       Collection<Action> actions = new ArrayList<Action>();
+       Action action = new AlignAction.Horizontal(new DefaultDrawingEditor());
+       actions.add(action);
+
+       //Check Action does not consist in the current Collection
+       assertTrue(!selectionTool.getFigureActions().contains(action));
+
+       // Insert Collection of Actions in the DelegationSelectionTool
+       selectionTool.setFigureActions(actions);
+
+        // Check if the Action exist in the Collection
+       assertTrue(selectionTool.getFigureActions().contains(action));
+
+    }
+
+}
