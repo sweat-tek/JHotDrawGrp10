89aef8979a65a4de134f3ad7c6fe4f3ca0825f76
tobber00 <39513708+tobber00@users.noreply.github.com>
2023-01-03 09:32:33 +0100

    finsihed draw

--- jhotdraw-samples/jhotdraw-samples-misc/src/main/java/org/jhotdraw/samples/svg/figures/SVGPathFigure.java
+++ jhotdraw-samples/jhotdraw-samples-misc/src/main/java/org/jhotdraw/samples/svg/figures/SVGPathFigure.java
@@ -86,25 +86,32 @@ public class SVGPathFigure extends AbstractAttributedCompositeFigure implements 
                 Rectangle2D.intersect(drawingArea, clipBounds, drawingArea);
             }
             if (!drawingArea.isEmpty()) {
-                BufferedImage buf = new BufferedImage(
-                        Math.max(1, (int) ((2 + drawingArea.width) * g.getTransform().getScaleX())),
-                        Math.max(1, (int) ((2 + drawingArea.height) * g.getTransform().getScaleY())),
-                        BufferedImage.TYPE_INT_ARGB);
-                Graphics2D gr = buf.createGraphics();
-                gr.scale(g.getTransform().getScaleX(), g.getTransform().getScaleY());
-                gr.translate((int) -drawingArea.x, (int) -drawingArea.y);
-                gr.setRenderingHints(g.getRenderingHints());
+                Graphics2D gr = grConf(drawingArea, g);
                 drawFigure(gr);
                 gr.dispose();
                 Composite savedComposite = g.getComposite();
                 g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, (float) opacity));
-                g.drawImage(buf, (int) drawingArea.x, (int) drawingArea.y,
+                g.drawImage(bufferedImageMaker(drawingArea, g), (int) drawingArea.x, (int) drawingArea.y,
                         2 + (int) drawingArea.width, 2 + (int) drawingArea.height, null);
                 g.setComposite(savedComposite);
             }
         } else if (opacity != 0d) {
             drawFigure(g);
         }
+    }
+    private Graphics2D grConf(Rectangle2D.Double drawingArea, Graphics2D g) {
+        Graphics2D gr = bufferedImageMaker(drawingArea, g).createGraphics();
+        gr.scale(g.getTransform().getScaleX(), g.getTransform().getScaleY());
+        gr.translate((int) -drawingArea.x, (int) -drawingArea.y);
+        gr.setRenderingHints(g.getRenderingHints());
+        return gr;
+    }
+    private BufferedImage bufferedImageMaker(Rectangle2D.Double drawingArea, Graphics2D g){
+        BufferedImage buf = new BufferedImage(
+                Math.max(1, (int) ((2 + drawingArea.width) * g.getTransform().getScaleX())),
+                Math.max(1, (int) ((2 + drawingArea.height) * g.getTransform().getScaleY())),
+                BufferedImage.TYPE_INT_ARGB);
+        return buf;
     }
 
 
